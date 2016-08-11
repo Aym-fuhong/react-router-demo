bcde14af9e31e4e9e5029fbfe6091d103e82d3b6
Yvette <libra1014tyb@163.com>
2016-08-11 17:49:55 +0800

    react-router-demo

--- app.js
+++ app.js
@@ -1,23 +1,81 @@
 const App = React.createClass({
+    getInitialState: function () {
+        return {
+            elements: []
+        };
+    },
+    removeElement: function (index) {
+        const elements = this.state.elements;
+        elements.splice(index, 1);
+        this.setState({elements});
+    },
+    addElement:function (element) {
+        const elements = this.state.elements;
+        elements.push(element);
+        this.setState({elements});
+    },
     render: function () {
-        return <div>{this.props.children}</div>;
+        return <div>
+            {this.props.children && React.cloneElement(this.props.children, {
+                elements: this.state.elements,
+                onRemove: this.removeElement,
+                onAdd:this.addElement
+            })}
+        </div>;
     }
 });
 
-const Editor =React.createClass({
-    render:function () {
+const Editor = React.createClass({
+    render: function () {
         return <div>
             <ReactRouter.Link to="/previewer">Previewer</ReactRouter.Link>
-            Editor
+            <LeftPanel elements={this.props.elements} onRemove={this.props.onRemove}/>
+            <RightPanel onAdd={this.props.onAdd}/>
         </div>;
     }
 })
 
-const Previewer =React.createClass({
-    render:function () {
+const LeftPanel = React.createClass({
+    remove: function (index) {
+        this.props.onRemove(index);
+    },
+    render: function () {
+        const elements = this.props.elements.map((ele, index)=> {
+            return <div key={index}>
+                <input type={ele}/>
+                <button onClick={this.remove.bind(this, index)}>X</button>
+            </div>
+        })
+        return <div>
+            {elements}
+        </div>;
+    }
+})
+
+const RightPanel = React.createClass({
+    add: function () {
+        const element=$("input[name=element]:checked").val();
+        this.props.onAdd(element);
+    },
+    render: function () {
+        return <div>
+            <input type="radio" name="element" value='text'/>Text
+            <input type="radio" name="element" value='date'/>Date
+            <button onClick={this.add}>+</button>
+        </div>;
+    }
+})
+
+const Previewer = React.createClass({
+    render: function () {
+        const elements = this.props.elements.map((ele,index)=>{
+            return <div key={index}>
+                <input type={ele}/>
+            </div>
+        })
         return <div>
             <ReactRouter.Link to="/">Edit</ReactRouter.Link>
-            Previewer</div>;
+            {elements}</div>;
     }
 })
 
